775803a689b1e3e6c18bc1eda199221d7ad25b23
mamjerez <correomam@gmail.com>
2018-12-01 22:31:09 +0100

    Limpio y ordeno codigo.

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,39 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService } from '../../services/heroes.service';
 
-
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styles: []
 })
+
 export class HeroesComponent implements OnInit {
 
   heroes: any[] = [];
-  loading:boolean = true;
+  loading = true;
 
   constructor(private _heroesService: HeroesService) {
-        this._heroesService.getHeroes()
-        .subscribe( data => {
-          this.heroes = data;
-          this.loading = false;
-        });
+    this._heroesService.getHeroes()
+      .subscribe(data => {
+        this.heroes = data;
+        this.loading = false;
+      });
   }
 
   ngOnInit() {
   }
 
-  borraHeroe( key$: string){
-
+  borraHeroe(key$: string) {
     this._heroesService.borrarHeroe(key$)
-        .subscribe( respuesta => {
-          if ( respuesta ) {
-            console.error(respuesta);
-          } else {
-            // todo bien
-            delete this.heroes[key$];
-          }
-        });
+      .subscribe(respuesta => {
+        if (respuesta) {
+          console.error(respuesta);
+        } else {
+          // todo bien
+          delete this.heroes[key$];
+        }
+      });
   }
 
 }
